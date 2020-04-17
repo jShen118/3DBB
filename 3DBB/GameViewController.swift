8596b21b6442588e308bcb9040c7ec0d5880f565
Alex Marrinan <alex@balroe.com>
2020-04-17 11:54:50 -0400

    Ball bouncing around simple box

--- 3DBB/GameViewController.swift
+++ 3DBB/GameViewController.swift
@@ -33,13 +33,64 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(ambientLightNode)
         
         // retrieve the ship node
-        //let room = scene.rootNode.childNode(withName: "room", recursively: true)!
-        let ball = scene.rootNode.childNode(withName: "sphere", recursively: true)
-        //room.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
-        ball?.physicsBody?.velocity = SCNVector3(x: 0, y: 0, z: -25)
+        let ball = scene.rootNode.childNode(withName: "ball", recursively: false)
+        ball?.physicsBody?.velocity = SCNVector3(x: 10, y: -30, z: 10)
         
-        
-        
+        let boxBottom = SCNBox(width: 13, height: 1, length: 13, chamferRadius: 0.2)
+        boxBottom.firstMaterial?.diffuse.contents = UIColor.blue
+        let boxBottomNode = SCNNode(geometry: boxBottom)
+        scene.rootNode.addChildNode(boxBottomNode)
+        boxBottomNode.position = SCNVector3(x: 1, y: -2, z: 1)
+        boxBottomNode.physicsBody = SCNPhysicsBody.static()
+        boxBottomNode.physicsBody?.restitution = 1.0
+        boxBottomNode.physicsBody?.friction = 0
+
+        let boxTop = SCNBox(width: 13, height: 1, length: 13, chamferRadius: 0.2)
+        boxTop.firstMaterial?.diffuse.contents = UIColor.blue
+        let boxTopNode = SCNNode(geometry: boxTop)
+        scene.rootNode.addChildNode(boxTopNode)
+        boxTopNode.position = SCNVector3(x: 1, y: 12, z: 1)
+        boxTopNode.physicsBody = SCNPhysicsBody.static()
+        boxTopNode.physicsBody?.restitution = 1.0
+        boxTopNode.physicsBody?.friction = 0
+
+        let boxLeft = SCNBox(width: 1, height: 13, length: 13, chamferRadius: 0.2)
+        boxLeft.firstMaterial?.diffuse.contents = UIColor.blue
+        let boxLeftNode = SCNNode(geometry: boxLeft)
+        scene.rootNode.addChildNode(boxLeftNode)
+        boxLeftNode.position = SCNVector3(x: -5, y: 5, z: 1)
+        boxLeftNode.physicsBody = SCNPhysicsBody.static()
+        boxLeftNode.physicsBody?.restitution = 1.0
+        boxLeftNode.physicsBody?.friction = 0
+
+        let boxRight = SCNBox(width: 1, height: 13, length: 13, chamferRadius: 0.2)
+        boxRight.firstMaterial?.diffuse.contents = UIColor.blue
+        let boxRightNode = SCNNode(geometry: boxRight)
+        scene.rootNode.addChildNode(boxRightNode)
+        boxRightNode.position = SCNVector3(x: 7, y: 5, z: 1)
+        boxRightNode.physicsBody = SCNPhysicsBody.static()
+        boxRightNode.physicsBody?.restitution = 1.0
+        boxRightNode.physicsBody?.friction = 0
+
+        let boxBack = SCNBox(width: 12, height: 13, length: 1, chamferRadius: 0.2)
+        boxBack.firstMaterial?.diffuse.contents = UIColor.blue
+        let boxBackNode = SCNNode(geometry: boxBack)
+        scene.rootNode.addChildNode(boxBackNode)
+        boxBackNode.position = SCNVector3(x: 1, y: 5, z: -5)
+        boxBackNode.physicsBody = SCNPhysicsBody.static()
+        boxBackNode.physicsBody?.restitution = 1.0
+        boxBackNode.physicsBody?.friction = 0
+
+        let boxFront = SCNBox(width: 12, height: 13, length: 1, chamferRadius: 0.2)
+        boxFront.firstMaterial?.diffuse.contents = UIColor.blue
+        let boxFrontNode = SCNNode(geometry: boxFront)
+        scene.rootNode.addChildNode(boxFrontNode)
+        boxFrontNode.position = SCNVector3(x: 1, y: 5, z: 7)
+        boxFrontNode.physicsBody = SCNPhysicsBody.static()
+        boxFrontNode.physicsBody?.restitution = 1.0
+        boxFrontNode.opacity = 0.4
+        boxFrontNode.physicsBody?.friction = 0
+
         // animate the 3d object
        //room.physicsBody?.applyForce(SCNVector3(x: 0, y: 20, z: 0), asImpulse: true) //room.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
         
